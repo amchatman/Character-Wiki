57c1536bd3dfe4e23ccb4138e8e9a836962e0384
Ashley Chatman <ashchat2008@gmail.com>
2023-06-14 10:58:09 -0400

    Nav Menu -Created ids -Created Event -Update Nav section

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
+const navButton = document.getElementById('nav-button');
+const navMenu = document.getElementById('nav-menu');
+
+navMenu.classList.add('hidden');
+
+navButton.addEventListener('click', function(){
+    navMenu.classList.toggle('hidden')
+
+})
+
 console.log("hello");
