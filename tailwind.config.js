2957fe3093ab87a4fc486d559f52c772997953de
Ashley Chatman <ashchat2008@gmail.com>
2023-06-26 15:05:11 -0400

    Back-Image & URL -Add backgroundImage -Update bg color -Update anchor tag & link

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'game-cover': "url('/src/img/MassEffect.jpg')",
+      }
+    },
   },
   plugins: [],
 }
